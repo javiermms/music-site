5f5f7cbf2d8f32e7928355cd01228e19dbb3e023
Javier Mendoza <jm9504202@gmail.com>
2020-01-28 16:15:30 -0800

    created album class

--- music/models.py
+++ music/models.py
@@ -6,10 +6,17 @@ class Musician(models.Model):
     def __str__(self):
         return self.name
 
+class Album(models.Model):
+     name = models.CharField(max_length=100)
+     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+     genre = models.CharField(max_length=50)
+
 class Song(models.Model):
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    num_stars = models.IntegerField()
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, null = True)
+   
+   
 
     def __str__(self):
         return self.name
